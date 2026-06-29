c4063548893d7cd4dd03b107454b22b69c2ac660
Mayank121805 <mayankkarki1818@gmail.com>
2026-06-29 10:37:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,17 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-key-123'
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = '1234'
-    MYSQL_DB = 'student_analyzer'
-    UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static', 'uploads')
-    MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16 MB max limit
+
+    MYSQL_HOST = os.getenv('MYSQLHOST')
+    MYSQL_USER = os.getenv('MYSQLUSER')
+    MYSQL_PASSWORD = os.getenv('MYSQLPASSWORD')
+    MYSQL_DB = os.getenv('MYSQLDATABASE')
+    MYSQL_PORT = int(os.getenv('MYSQLPORT', 3306))
+
+    UPLOAD_FOLDER = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        'static',
+        'uploads'
+    )
+
+    MAX_CONTENT_LENGTH = 16 * 1024 * 1024
