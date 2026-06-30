7152b94e6fc10189e9fa3d06452699c42eedc873
Mayank121805 <mayankkarki1818@gmail.com>
2026-06-30 12:32:46 +0530

    Remove MySQL configuration from Config class
    Removed MySQL configuration variables from Config class.

--- config.py
+++ config.py
@@ -2,13 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-key-123'
-
-    MYSQL_HOST = os.getenv('MYSQLHOST')
-    MYSQL_USER = os.getenv('MYSQLUSER')
-    MYSQL_PASSWORD = os.getenv('MYSQLPASSWORD')
-    MYSQL_DB = os.getenv('MYSQLDATABASE')
-    MYSQL_PORT = int(os.getenv('MYSQLPORT', 3306))
-
     UPLOAD_FOLDER = os.path.join(
         os.path.dirname(os.path.abspath(__file__)),
         'static',
